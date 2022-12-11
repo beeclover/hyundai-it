fe9ab3d1c9b02f20a98bcf2fd6976c791d11a52d
Hansanghyeon <999@hyeon.pro>
2022-12-11 14:58:29 +0000

    feat(tailwindcss): 공용 tailwind config

---
+++ packages/tailwindcss/tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      container: {
+        center: true,
+        padding: "20px",
+      },
+      fontFamily: {
+        ng: ["niveau-grotesk"],
+        pretendard: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Apple SD Gothic Neo",
+          "Pretendard Variable",
+          "Pretendard",
+          "Roboto",
+          "Noto Sans KR",
+          "Segoe UI",
+          "Malgun Gothic",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "sans-serif",
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
