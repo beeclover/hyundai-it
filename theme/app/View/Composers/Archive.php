20176bf76c2ecd4c3b3993dd9a1f54af586d87c8
Hansanghyeon <999@hyeon.pro>
2022-12-29 17:12:51 +0000

    feat(sage): custom parameter로 tax_query추가

--- theme/app/View/Composers/Archive.php
+++ theme/app/View/Composers/Archive.php
@@ -36,3 +36,37 @@ class Archive extends Composer
     return get_the_archive_title();
   }
 }
+
+// FAQ, 다운로드 taxonomy, 키워드 검색
+
+add_filter('query_vars', function ($vars) {
+    $vars[] = 'download-taxonomy';
+    $vars[] = 'search';
+    return $vars;
+});
+
+add_action( 'pre_get_posts', function ( $query ) {
+    if (
+        $query->is_main_query()
+        && !is_admin()
+        && is_archive()
+        && get_query_var('product')
+    ) {
+        $query->set('tax_query', array(
+            array(
+                'taxonomy' => 'product',
+                'field' => 'slug',
+                'terms' => get_query_var('product'),
+            ),
+        ));
+    }
+
+    if (
+        $query->is_main_query()
+        && !is_admin()
+        && is_archive()
+        && get_query_var('search')
+    ) {
+        $query->set('s', get_query_var('search'));
+    }
+} );
