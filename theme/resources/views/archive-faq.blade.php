0cbf83b61acd231a7895dfacb7cb82f054658fd5
Hansanghyeon <999@hyeon.pro>
2022-12-30 16:47:18 +0000

    feat(sage): 다운로드, FAQ 동일구조로 맞춤

--- theme/resources/views/archive-faq.blade.php
+++ theme/resources/views/archive-faq.blade.php
@@ -1,23 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-  <x-page-header title="{!! $title !!}" thumbnail="{!! $thumbnail !!}" parent_menu="{!! $parent_menu !!}" />
-  @if (!have_posts())
-    <x-C404 />
-  @else
-    <div data-component="spacer" class="h-[90px]"></div>
-    <div class="container">
-      {{-- content --}}
-      <div x-data="faq" class="grid grid-cols-[min-content_max-content_max-content_minmax(367px,1fr)] gap-y-px bg-[#707070] border border-[#707070]">
-        {{-- row start --}}
-        @while(have_posts()) @php(the_post())
+  <x-page-header title="{!! $title !!}" thumbnail="{!! $thumbnail !!}" parentMenu="{!! $parent_menu !!}" />
+  <div data-component="spacer" class="h-[90px]"></div>
+  <div class="container">
+    {{-- head --}}
+    @include('partials.archive-search')
+    <x-spacer class="h-[30px]" />
+    {{-- head end --}}
+    {{-- content --}}
+    <div x-data="faq" class="grid grid-cols-[min-content_max-content_max-content_minmax(367px,1fr)] gap-y-px bg-[#707070] border border-[#707070]">
+      {{-- row start --}}
+      @if (!have_posts())
+        <div class="col-sapn-3 bg-white flex">
+          <x-C404 />
+        </div>
+      @endif
+      @while(have_posts()) @php(the_post())
         @includeFirst(['partials.content-faq'])
-        @endwhile
-        {{-- row end --}}
-      </div>
-      <div data-component="spacer" class="h-[60px]"></div>
-      {!! $pagination !!}
+      @endwhile
+      {{-- row end --}}
     </div>
-  @endif
+    <div data-component="spacer" class="h-[60px]"></div>
+    {!! $pagination !!}
+  </div>
 @endsection
 
