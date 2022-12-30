08e9eec05ca1b072a3f196e361d42c77ec948fc4
Hansanghyeon <999@hyeon.pro>
2022-12-30 16:04:54 +0000

    feat(sage): download single content template

---
+++ theme/resources/views/partials/content-download.blade.php
@@ -0,0 +1,15 @@
+<div class="h-[70px]"></div>
+<div class="pl-[68px] flex items-center bg-white" >
+  <b>{!! $제품 !!}</b>
+</div>
+<div class="px-[50px] flex items-center bg-white" >
+  <b>{!! $분류 !!}</b>
+</div>
+<div class="flex relative items-center pr-[30px] bg-white" >
+  <div class="text-left w-full" >
+    {!! $title !!}
+  </div>
+  <a href="" class="bg-[#5a5a5a] text-white text-[14px] pt-[8px] pb-[10px] px-[20px] break-keep rounded-[4px] font-bold">
+    다운로드
+  </a>
+</div>
