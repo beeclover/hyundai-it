9c469cd34b3194f2006718bc8171ba7cd80ab6b8
Hansanghyeon <999@hyeon.pro>
2022-12-31 14:21:49 +0000

    feat(sage): casestudy-tag 템플릿은 casestudy archive 템플릿으로 지정

--- theme/resources/views/archive-download.blade.php
+++ theme/resources/views/archive-download.blade.php
@@ -3,10 +3,15 @@
  --}}
 @extends('layouts.app')
 @section('content')
-  <x-page-header title="{!! $title !!}" thumbnail="{!! $thumbnail !!}" parentMenu="{!! $parent_menu !!}" />
+  <x-page-header
+    title="{!! $title !!}"
+    thumbnail="{!! $thumbnail !!}"
+    parentMenu="{!! $parent_menu !!}"
+    postType="{!! $post_type !!}"
+  />
   <x-spacer class="h-[90px]" />
   <div class="container">
-    @include('partials.archive-search')
+    @include('partials.header-search')
     <x-spacer class="h-[30px]" />
     {{-- content --}}
     <div class="grid grid-cols-[min-content_max-content_max-content_minmax(367px,1fr)] gap-y-px bg-[#707070] border border-[#707070]">
